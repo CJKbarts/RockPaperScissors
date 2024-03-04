a553ed1f2f46c45fce70dd219a8f3d10586347a4
CJKbarts <bartelsjacobcorn@gmail.com>
2024-03-04 15:28:57 -0600

    Add playGame function
    Runs playRound function 5 times
    Allows for an extra round if player draws
    Breaks out of loop if a player gets to 3 points
    Sends a message to inform who won overall
    
    Modified playRound to return true, false, or "Draw"

--- script.js
+++ script.js
@@ -21,22 +21,57 @@ function getComputerChoice (){
 
 function playRound (playerChoice, computerChoice){
     playerChoice = playerChoice.toLowerCase();
-    let winMessage = `You win! ${playerChoice} beats ${computerChoice}`;
-    let loseMessage = `You lose! ${computerChoice} beats ${playerChoice}`;
 
     if (playerChoice === computerChoice) return "Draw";
     else if (playerChoice === "rock") {
-        if (computerChoice === "scissor") return winMessage;
-        else return loseMessage;
+        if (computerChoice === "scissors") return true;
+        else return false;
     }
 
     else if (playerChoice === "paper") {
-        if (computerChoice === "rock") return winMessage;
-        else return loseMessage;
+        if (computerChoice === "rock") return true;
+        else return false;
     }
 
     else if (playerChoice === "scissors") {
-        if (computerChoice === "paper") return winMessage;
-        else return loseMessage;
+        if (computerChoice === "paper") return true;
+        else return false;
     }
+}
+
+function playGame (){
+    let playerScore = 0;
+    let computerScore = 0;
+
+    let playerChoice;
+    let computerChoice;
+    
+
+    for(let i = 0; i < 5; i++){
+        playerChoice = prompt("Rock, Paper, or Scissors: ");
+        computerChoice = getComputerChoice();
+        
+        let result = playRound(playerChoice, computerChoice);
+        let message = result? `You win! ${playerChoice} beats ${computerChoice}` : 
+            `You lose! ${computerChoice} beats ${playerChoice}`;
+
+        if (result === "Draw") {
+            i--;
+            console.log("Draw! Play again!")
+            continue;
+        }
+
+        if (result){
+            playerScore++;
+        }
+
+        else {
+            computerScore++;
+        }
+        console.log(message)
+
+        if (playerScore === 3 || computerScore === 3) break;
+    }
+
+    console.log((playerScore > computerScore) ? "You win!": "You lose!")
 }
